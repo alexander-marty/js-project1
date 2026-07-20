a8d0cd741c323a31cdbcc73b06cb548b2ae340a6
Martynenko <omartynenko@credolab.com>
2026-07-20 13:37:55 +0300

    Restarted course

---
+++ lesson-14.js
@@ -0,0 +1,12 @@
+const isSuited = 100 - 10 > 90 - 5;
+console.log(isSuited); 
+
+const a = (6 + 10) / 2;
+console.log(a);
+
+let b;
+let c;
+b = 100;
+c = b + 100 + 50;
+console.log(b);
+console.log(c);
--- lesson32.js
+++
@@ -1,9 +0,0 @@
-function powerOfTwo (num){
-    return num * num;
-}
-console.log(powerOfTwo(5));
-
-const powt = function (num) {
-    return num * num;
-}
-console.log(powt(6));
