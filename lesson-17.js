033434e90e8e207184cbf560e6a0c870cfddf259
Martynenko <omartynenko@credolab.com>
2026-09-16 11:19:18 +0300

    Finished Basics

---
+++ lesson-17.js
@@ -0,0 +1,9 @@
+const projectName = "Shop Site";
+const price = 2000;
+const author = "Basil";
+
+const template = author + ' ordered ' + projectName + ' by price ' + price;
+console.log(template);
+
+const template2 = `${author} ordered ${projectName} by price ${price}` 
+console.log(template2);
---
+++ lesson-18.js
@@ -0,0 +1,18 @@
+const age = '18';
+console.log(Number(age) + 5);
+console.log(age - 3);
+const userName = 'Basil';
+console.log(Number(userName) + 5);
+
+console.log(String(4 + 7));
+
+//All numbers except 0 are considered true
+console.log(Boolean(1));
+console.log(Boolean(0));
+
+//Empty string is always false
+console.log(Boolean(''));
+console.log(Boolean('userName'));
+
+const a = 2 + '10';
+console.log(a - 10);
